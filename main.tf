932dec2d834ba98bff7d5c2209f4f22165385365
Cyril Dubuisson <cyril.dubuisson@gmail.com>
2019-06-18 11:30:34 +0200

    :bug: : keep docker image for destroy operation

--- main.tf
+++ main.tf
@@ -5,8 +5,9 @@ resource "docker_image" "mongo" {
 
 # start a container and expose the 27017 port
 resource "docker_container" "mongo" {
-  name  = "${var.mongo_container_name}"
-  image = "${docker_image.mongo.latest}"
+  name         = "${var.mongo_container_name}"
+  image        = "${docker_image.mongo.latest}"
+  keep_locally = true
   ports = {
     internal = 27017
     external = "${var.mongo_exposed_port}"
