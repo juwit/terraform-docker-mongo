6758c07597e25a8a622f0485fb4c58a662e0f690
Cyril Dubuisson <cyril.dubuisson@gmail.com>
2019-06-18 12:13:00 +0200

    :bug: : move keep_locally in docker_image instead of docker_container

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # get the mongo docker image
 resource "docker_image" "mongo" {
-  name = "mongo"
+  name         = "mongo"
+  keep_locally = true
 }
 
 # start a container and expose the 27017 port
 resource "docker_container" "mongo" {
-  name         = "${var.mongo_container_name}"
-  image        = "${docker_image.mongo.latest}"
-  keep_locally = true
+  name  = "${var.mongo_container_name}"
+  image = "${docker_image.mongo.latest}"
   ports = {
     internal = 27017
     external = "${var.mongo_exposed_port}"
